3b3fe18b675c9ed1f3e1dcceebaadcd6e44879fb
Mayen Padua <edmarenpadua@gmail.com>
2014-02-05 11:55:25 +0800

    mayen updated javascript

--- ICSLib/application/views/v_user.php
+++ ICSLib/application/views/v_user.php
@@ -311,7 +311,7 @@
 	<!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
 
